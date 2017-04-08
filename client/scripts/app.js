04115a24e1df1e16cb9320fd93b7b3cd7e6e2c62
Hack Reactor Student <student@hackreactor.com>
2017-04-08 12:36:48 -0700

    working on post message

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,44 +1,48 @@
-// ********************** EXAMPLE POST MESSAGE **********************************
-// var message = {
-//   username: 'shawndrost',
-//   text: 'trololo',
-//   roomname: '4chan'
-// };
-// $(document).ready(function() {
-//  app.init();
-// });
+$(document).ready(function() {
+  app.init();
 
-// var message = {
-//   username: 'shawndrost',
-//   text: 'trololo',
-//   roomname: '4chan'
-// };
+  $('#postMessage').on('click', function(event) {
+    event.preventDefault();
+    console.log('we got the click');
+    var $text = $('#messageText').val();
+    var $url = $(location).attr('href');
+    var $username //= get the username from the url
+    var $roomname //= get the room from the url
+
+    var $message = {
+      username: $username,
+      text: $text,
+      roomname: $roomname
+    }
+
+    app.send($message);
+  });
+
+});
 
 var app = {};
 
 app.init = () => {
   app.fetch();
+  console.log(app.fetch())
 };
 
 app.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
 
 // Two lines below are for Message implementation
-var test = () => {
-  var postedMessage = document.getElementsById('#messageText').value;
-  console.log('this is the message text: ', postedMessage, 'hello' );
 
-}
 
 // Dropdown functionality ______________________________________________
 
 var myFunction = () => {
   document.getElementById('myDropdown').classList.toggle('show');
 }
+
 window.onclick = function(event) {
   if (event.target.matches('#lobby')) {
-    app.fetch()
-  } else if (event.target.matches('#spaceball')) {
-    app.clearMessages(); 
+    app.fetch( 'lobby' )
+  } else if (event.target.matches('#China')) {
+    app.fetch('China') 
   } else if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
@@ -70,7 +74,7 @@ app.send = (message) => {
     data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
-      console.log('this is the children length ', $('#chats').children().length);
+      app.renderMessage(message);
       console.log('chatterbox: Message sent');
     },
     error: function (data) {
@@ -80,16 +84,22 @@ app.send = (message) => {
   });
 };
 
-app.fetch = () => {
+app.fetch = ( nameOfRoom ) => {
   $.ajax({
     url: app.server,
     type: 'GET',
     data: {},
     contentType: 'application/json',
     success: function (data) {
-      //console.log(data.results).   <-- shows me roomName
-      for (var i = data.results.length - 1; i >= 0; i--) {
-        $('#chats').append('<div>' + data.results[i].username + ': ' + data.results[i].text + ': ' + data.results[i].roomname + '</div>');
+      // nameOfRoom = nameOfRoom || data.results;
+      var filteredArr = _.filter(data.results, function(dataObj) {
+        return dataObj.roomname === nameOfRoom
+      });
+
+      app.clearMessages();
+
+      for (let message of filteredArr) {
+        $('#chats').prepend('<div>' + message.username + ': ' + message.text + ': ' + message.roomname + '</div>' + '<br>');
       }
       console.log('chatterbox: Messages received');
     },
@@ -104,12 +114,14 @@ app.clearMessages = () => {
   $('#chats').empty();
 };
 
-var message = {
-  text: 'Yo';
-}
+// var message = {
+//   text: 'Yo',
+//   username: 'Jonathan',
+//   room: 'lobby'
+// }
+
 app.renderMessage = (message) => {
-  // app.send(message);
-  $('#chats').append('<div>' + message.text + '</div>');
+  $('#chats').prepend('<div>' + message.text + '</div>');
 };
 
 // JT creates Add Room functionality to add to #roomSelect Div
@@ -118,13 +130,12 @@ app.renderRoom = (makeRoom) => {
   $('#roomSelect').append('<div>' + makeRoom + '</div>');
 }
 
-app.init();
 
-
-
-
-
-
+// if lobby was selected from dropDown
+  // fetch all data
+  // app.fetch = (  'room' )
+    // newArr = [];
+    // filter all objects with roomname === lobby.  //('room')
 
 
 
